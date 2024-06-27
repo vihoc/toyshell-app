3e7351220ae43d7a79bfeeb65c5bea599098c439
vihoc <chenwenhao@hiroshis-MacBook-Pro.local>
2024-06-27 10:05:58 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,12 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  while(true){
 
-  std::cout << "$ ";
+    std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
-
+    std::string input;
+    std::getline(std::cin, input);
+    std::cout << input << ": " << "command not found" << std::endl;
+  }
 }
