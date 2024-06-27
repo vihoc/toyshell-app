3390ab8c93db9b41935b2bcc3ae06a5c063262e1
vihoc <chenwenhao@hiroshis-MacBook-Pro.local>
2024-06-27 11:01:29 -0700

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,61 @@
 #include <iostream>
 #include <string>
+#include <vector>
+bool is_exit(std::string& input){
+  if(0 == input.find("exit"))
+  {
+    return true;
+  }
+  return false;
+}
+
+void handle_echo(std::string& input){
+  if(input.length() >= 5){
+    std::cout << input.substr(5) << std::endl;
+  }
+  else
+  {
+    std::cout << std::endl;
+  }
+}
+
+void handle_type(std::string& input)
+{
+  std::vector<std::string> shellbuildin = {"echo", "type", "exit"};
+  if(input.length() < 5)
+  {
+    std::cout << "type: " << "missing argument" << std::endl;
+    return;
+  }
+  std::string sub = input.substr(5);
+  for (std::string& command : shellbuildin)
+  {
+    
+    if(0 == sub.find(command))
+    {
+      std::string sub = input.substr(5);
+      std::cout << sub << " is a shell builtin" << std::endl;
+      return;
+    }
+  }
+  std::cout << sub << ": not found" << std::endl;
+
+
+}
+bool is_handled(std::string& input){
+  if(0 == input.find("echo"))
+  {
+    handle_echo(input);
+    return true;
+  }
+  else if(0 == input.find("type"))
+  {
+    handle_type(input);
+    return true;
+  }
+  else return false;
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -10,14 +66,14 @@ int main() {
 
     std::string input;
     std::getline(std::cin, input);
-    if(0 == input.find("exit"))
+    if(is_exit(input))
     {
       break;
     }
     else{
-      if(0 == input.find("echo "))
+      if(is_handled(input))
       {
-          std::cout << input.substr(5) << std::endl;
+        ;
       }
       else{
         std::cout << input << ": " << "command not found" << std::endl;
